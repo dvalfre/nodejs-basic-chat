097dcd11e1aa77a5d1a97a91357ae6160c878be5
Mikong Galero <mgalero@engineyard.com>
2016-07-26 21:09:53 +0800

    Add chat view. Render with Pug.

--- app.js
+++ app.js
@@ -3,10 +3,16 @@ var app = express();
 var http = require('http').Server(app);
 var port = process.env.PORT || 3000;
 
+// Configure app
+app.set('views', './views');
+app.set('view engine', 'pug');
+
+// Routes
 app.get('/', function(req, res) {
-  res.send('<h1>Hello Engine Yard Cloud!</h1>');
+  res.render('index');
 });
 
+
 http.listen(port, function() {
-  console.log("Express server listening on %d in %s mode", port, app.settings.env)
+  console.log("Express server listening on %d in %s mode", port, app.settings.env);
 });
